5647c7f1ceaca448440aab9ef074865a244b3d60
Zehan Khan <zkhan@hootboard.com>
2023-11-11 03:10:52 +0530

    feat: :sparkles: add env variables, config file and joi to validate env variable

---
+++ src/config/config.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import path from "path";
+import Joi from "joi";
+
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+console.log(process.env.PORT);
+
+const envSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string()
+      .valid("production", "development", "test")
+      .required(),
+    PORT: Joi.number().default(3000),
+    MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+  })
+  .unknown();
+
+const { value: envVars, error } = envSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+export const config = {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  mongoose: {
+    url: envVars.MONGODB_URL + (envVars.NODE_ENV === "test" ? "-test" : ""),
+  },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { Server } from "http";
 import app from "./app";
-const PORT = 3000;
+import { config } from "./config/config";
+
+const PORT = config.port || 3000;
 
 const server: Server = app.listen(PORT, (): void => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
